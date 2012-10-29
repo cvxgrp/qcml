8ced8b3abded3e3d6b3ced0132e159b32d3c7d2b
Eric Chu <eytchu@gmail.com>
2012-10-29 13:43:50 -0700

    rewrote portfolio example for better numbers

--- matlab/ecos_tester.m
+++ matlab/ecos_tester.m
@@ -7,18 +7,18 @@ function ecos_tester()
     cvx_solver sedumi
 
     try 
-%         run_test('least_squares'); fprintf('\n');
-%         run_test('geometric_mean'); fprintf('\n');
-%         run_test('lp'); fprintf('\n');
-%         run_test('quadratic_over_linear'); fprintf('\n');
-%         run_test('inv_prob'); fprintf('\n');
-%         run_test('min_max'); fprintf('\n');
-%         run_test('robust_ls'); fprintf('\n');
-%         run_test('ecos_mpc'); fprintf('\n');
-%         run_test('lasso'); fprintf('\n');
+        run_test('least_squares'); fprintf('\n');
+        run_test('geometric_mean'); fprintf('\n');
+        run_test('lp'); fprintf('\n');
+        run_test('quadratic_over_linear'); fprintf('\n');
+        run_test('inv_prob'); fprintf('\n');
+        run_test('min_max'); fprintf('\n');
+        run_test('robust_ls'); fprintf('\n');
+        run_test('ecos_mpc'); fprintf('\n');
+        run_test('lasso'); fprintf('\n');
         run_test('portfolio'); fprintf('\n');
-%         run_test('svm'); fprintf('\n');
-%         run_test('chebyshev'); fprintf('\n');
+        run_test('svm'); fprintf('\n');
+        run_test('chebyshev'); fprintf('\n');
 
 
     catch e
@@ -41,7 +41,7 @@ function run_test(directory)
     
 
     tic;
-    [status, result] = system(['../../src/ProbToCVX --ecos ' directory '.prob']);
+    [status, result] = system(['../../src/efe --ecos ' directory '.prob']);
     fprintf('  ecos rewrite time %f\n', toc);
 
     clear x
