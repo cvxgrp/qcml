34f15d299341b5ea972f65e0ac25f0a0b60e00a9
Eric Chu <eytchu@gmail.com>
2013-02-04 10:51:00 -0800

    some notes to self about atoms and parameterless problems

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -2,7 +2,7 @@ from scoop import Scoop
 from profiler import print_prof_data
 
 # temporarily expose this
-from scoop_atoms import Evaluator
+from scoop_atoms import Evaluator, Cone, Row
 from scoop_expression import Constant, Variable
 
 # to make sure atoms get loaded
--- src/python/scoop_atoms.py
+++ src/python/scoop_atoms.py
@@ -28,6 +28,8 @@ those of the authors and should not be interpreted as representing official
 policies, either expressed or implied, of the FreeBSD Project.
 """
 
+from scoop_expression import SCALAR, VECTOR, MATRIX
+
 # this is just a scaffolding for atoms
 class Evaluator(object):
     # counter for new variables introduced (private?)
@@ -39,12 +41,49 @@ class Evaluator(object):
     
     def hello(self):
         print self.symtable
-        
     
     def new_var(self,shape):
         name = 't' + str(self.varcount)
         self.varcount += 1
         return Variable(name, shape)
+
+class Row(object):
+    pass
+    
+class Cone(object):
+
+    def __init__(self, t, *args):
+        # args are tuples of Variable
+        # cone is (t, *args) \in SOC
+        # if t is scalar, then this is *one* cone
+        # if t is a vector, then these are multiple cones
+        if t.shape is SCALAR:
+            self.is_multi_cone = False 
+        else:
+            self.is_multi_cone = True
+        
+        self.t = t
+        
+        # if there are no remaining args, it's an LP cone
+        if args:
+            self.is_LP_cone = False
+            self.cone_variables = args
+        else:
+            self.is_LP_cone = True
+            self.cone_variables = ()
+    
+    def __repr__(self):
+        if self.is_LP_cone:
+            return self.t.name + ' >= 0'
+        elif self.is_multi_cone:
+            return 'norm(' + ', '.join(map(lambda x:x.name, self.cone_variables)) + ') <= ' + self.t.name
+        else:
+            return 'norm([' + '; '.join(map(lambda x:x.name, self.cone_variables)) + ']) <= ' + self.t.name
+            
+    def to_affine(self):
+        """Converts the cone constraint into a row of an affine equality constraint"""
+        # what about cone struct?
+        pass
 
 # (Coeff, Variable)? (Coeff,)
 
@@ -56,7 +95,8 @@ class Evaluator(object):
     # no, for the cones, we will form
     # (SCALAR Variable, Variable, Variable) # in (t,x,y,z) form
     # (VECTOR Variable, Variable) # this is multiarg form |x|<=y
-    # (Variable, ZERO) # this is x >= 0
+    # (VECTOR Variable, Variable, Variable) # norms([x y]) <= vec in R^n, norm is in R^2
+    # (Variable, ) # this is x >= 0
     # we'll pre-process this list before codegen, which will get us G, h
     # and will also form "struct" directly
         
@@ -85,3 +125,20 @@ class Evaluator(object):
 # EYE(start=1,stride=2) generates -> [0 0; 1 0; 0 0; 0 1]
     
 # otherwise, it will just consume using an RPN algorithm
+
+# another question: will i have to have the user provide the dimensions for the variables when they call the solver? i should. this would solve a lot of little problems.
+# okay, regarding above: here is the solution for languages with closures:
+#    p = Scoop()
+#    p.eval?/run?(...)
+#    f = p.generate()
+#    f(x_len=3,y_len=2)(A=1,b=2,c=3)   # this will solve it
+# dimensions are passed in using closures. so if variable dimensions change, you have to recall the whole function, but if only problem data changes, then you just have to call the second function.
+# so there's a distinction between problem dimensions and data
+
+# for languages without closures, you will have to do
+#    f(A=1,b=2,c=3,x_len=3,y_len=2), etc.
+# this is for something like C
+#
+# alternatively, for C, you could run a C-specific generator that requires knowledge of the dimensions before it generates code. that's fine, too. in fact, most C implementations will probably fall under this category. but they will also 
+#
+# obviously, i could work around this with C++ templating, but, hey, I'm not going to do that.
