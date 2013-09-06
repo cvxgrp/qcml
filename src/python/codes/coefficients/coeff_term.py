bfe84cf3c0d3b3352a16bab91c5ec4b239138abd
Eric Chu <eytchu@gmail.com>
2013-09-06 13:56:32 -0700

    new coeff is ready to merge

--- src/python/codes/coefficients/coeff_term.py
+++ src/python/codes/coefficients/coeff_term.py
@@ -10,6 +10,9 @@
     without having to write so much code.
     
     This file is just to keep track of that progress.
+    
+    TODO: This file is now complete and ready to mingle. Armed with this, we
+    need to simplify the coefficient.py module and the expressions/ modules.
 """
 from itertools import izip_longest
 
@@ -24,216 +27,66 @@ def profile(f):
     return wrap
 
 
-
-# ---- this is how it should be done ---- #
-# vararg add and multiply
-#
-# performance *is* an issue, since we create an Add and a Mul object *every
-# time*. however, the code reads nicer.
-#
-# now need to simplify "on the fly"
-
-class Identity(object):
-    def __mul__(self, other): return other
-    def __rmul__(self,other): return other
-    def __add__(self,other): return other
-    def __radd__(self,other): return other
-
-identity = Identity()
-
-
-class Expr(object):
-    def __init__(self, name):
-        self.name = name
-
-    # abstractmethod
-    def terms(self):
-        # additive terms
-        yield self
-
-    def factors(self):
-        # multiplicative factors (only parameters)
-        yield self
-
-    def variable(self):
-        # variable for a specific term
-        return
-        yield
-
-    def constant_term(self):
-        return identity
-
-    def constant_factor(self):
-        return identity
-    
-    def varstr(self):
-        """ The string of variables
-        """
-        return '*'.join(map(str, self.factors())) + '*' + '*'.join(map(str, self.variable()))
-
-    def __str__(self): return str(self.name)
-
-    def __add__(self, other):
-        # if isinstance(self, Addy):
-        #     if isinstance(other, Addy):
-        #         self.children.extend(other.children)
-        #     else:
-        #         self.children.append(other)
-        #     return self
-        # else:
-        #     if isinstance(other, Addy):
-        #         other.children.append(self)
-        #         return other
-        #     else:
-        #         return Addy(self, other)
-        # flatten add lists
-        
-        # collect constant * expr + expr into (constant + 1) * expr...
-        if self.varstr() == other.varstr():
-            c = 0
-            if other.constant_term() is identity: c += 1
-            else: c += other.constant_term()
-            if self.constant_term() is identity: c += 1
-            else: c += other.constant_term()
-            return Constant(c)*self
-        # if isinstance(self, Constant) and isinstance(other, Constant):
-        #     return Constant(self.name + other.name)
-        #     return Muly(Expr(2), self)
-        return Addy(self, other)
-
-    def __mul__(self, other):
-        # distribute
-        if isinstance(self, Addy):
-            return sum((c*other for c in self.children), Constant(0))
-        if isinstance(other, Addy):
-            return sum((self*c for c in other.children), Constant(0))
-        # if isinstance(self, Constant) and isinstance(other, Constant):
-        #     return Constant(self.name * other.name)
-        return Muly(self, other)
-
-    #__repr__ = __str__
-
-class Variable(Expr):
-    def __init__(self, value):
-        super(Variable, self).__init__(value)
-    
-    # abstractmethod
-    def terms(self):
-        # additive terms
-        yield self
-
-    def factors(self):
-        # multiplicative factors
-        yield identity
-    
-    def variable(self):
-        yield self
-
-    def constant_term(self):
-        return identity
-
-    def constant_factor(self):
-        return identity
-
-class Constant(Expr):
-    def __init__(self, value):
-        super(Constant, self).__init__(value)
-    
-    # abstractmethod
-    def terms(self):
-        return
-        yield
-
-    def factors(self):
-        return
-        yield
-
-    def constant_term(self):
-        if self.name == 0: return identity
-        else: return self.name
-
-    def constant_factor(self):
-        if self.name == 1: return identity
-        else: return self.name
-
-
-# TODO: flatten in the constructor...
-class Addy(Expr):
-    def __init__(self, left, right):
-        self._constant = left.constant_term() + right.constant_term()
-        self._children = list(arg for l in (left.terms(), right.terms()) for arg in l)
-        
-        for arg in left.terms():
-            print '---'
-            #print arg.constant_term()
-            if hasattr(arg, '_children'): print map(str,arg._children)
-        for arg in right.terms():
-            print '---'
-            #print arg.constant_term()
-            if hasattr(arg, '_children'): print map(str,arg._children)
-            
-        super(Addy, self).__init__('...')
-
-    def __str__(self):
-        return ' + '.join(map(str,self.children))
-
-    def terms(self):
-        # additive terms
-        for c in self._children:
-            yield c
-
-    def factors(self):
-        # multiplicative factors
-        yield self
-
-    def constant_term(self):
-        return self._constant
-
-    def constant_factor(self):
-        return identity
-
-    @property
-    def children(self):
-        if self._constant is not identity:
-            yield Constant(self._constant)
-        for e in self._children:
-            yield e
-
-    #__repr__ = __str__
-
-# how do i create a Mul object that only contains the variables / parameters?
-class Muly(Expr):
-    def __init__(self, left, right):
-        self._constant = left.constant_factor() * right.constant_factor()
-        self._children = list(arg for l in (left.factors(), right.factors()) for arg in l)
-        super(Muly, self).__init__('...')
-
-    def __str__(self):
-        return '*'.join(map(str,self.children))
-
-    def terms(self):
-        # additive terms
-        yield self
-
-    def factors(self):
-        # multiplicative factors
-        for c in self._children:
-            yield c
-
-    def constant_term(self):
-        return identity
-
-    def constant_factor(self):
-        return self._constant
-
-    @property
-    def children(self):
-        if self._constant is not identity:
-            yield Constant(self._constant)
-        for e in self._children:
-            yield e
-
 ###### STARTING OVER #######
+""" This module implements constant folding for expressions. It also collects
+    similar terms together, so x + x = 2*x.
+
+    However, it avoids collecting params + constant into coefficients. For
+    instance, 3*x + A*x != (diag(3) + A)*x. The reason is purely pragmatic.
+    
+    For C code generation, we would introduce a new variable for A*x. This
+    lets us copy the parameter "A" into a distinct location instead of
+    mixing its memory access with the diagonal memory access pattern.
+    
+    For Python code generation, we would actually execute diag(3) + A and
+    copy it into the proper location.
+"""
+import collections
+
+def term_updated(lhs_terms, rhs_terms):
+    """ This function is called once the variables have been matched.
+        Thus, we only need to match the params list.
+        
+        Will modify the matching element in-place in the lhs. 
+    """
+    for t1 in lhs_terms:
+        for t2 in rhs_terms:
+            if t1.params == t2.params:
+                t1.const_coeff += t2.const_coeff
+                return True
+    return False
+    
+
+class TermBucket(object):
+    """ Stores the terms as a bucket list.
+        
+        The variable name is the key, the key 1 is for parameters.
+    """
+    def __init__(self, buckets=None):
+        if buckets is None: self.term_bucket = collections.defaultdict(list)
+        else: self.term_bucket = buckets
+    
+    def __setitem__(self, k, v):
+        if k: self.term_bucket[k].append(v)
+        else: self.term_bucket[1].append(v)
+    
+    def __getitem__(self, k):
+        return self.term_bucket[k]
+    
+    def __add__(self, other):
+        buckets = collections.defaultdict(list)
+        for k,v in self.term_bucket.iteritems():
+            buckets[k].extend(v)
+        for k,v in other.term_bucket.iteritems():
+            # do a linear search on the terms to see if any match
+            if k in buckets and term_updated(buckets[k], v): continue
+            buckets[k].extend(v)
+        return TermBucket(buckets)
+    
+    def __iter__(self):
+        return (elem for sublist in self.term_bucket.values() for elem in sublist)
+        
+        
 
 """ Fundamentally, an "expression" is just a list of terms with each term
     represented by three things: a constant, a multiplication stack of
@@ -243,10 +96,8 @@ class Muly(Expr):
 """
 
 class Expression(object):
-    def __init__(self, constant, list_of_terms):
-        # self.terms = {term.variable:term for term in list_of_terms}
-        #   # make it a dict of terms? keyed by vars?
-        self.terms = list_of_terms
+    def __init__(self, constant, term_bucket):
+        self.terms = term_bucket
         self.constant = constant
     
     def __add__(self, other): return _Add(self, other)  # TODO: this is a func
@@ -254,11 +105,13 @@ class Expression(object):
     def __mul__(self, other): return _Mul(self, other)  # TODO: this is a func
     
     def __str__(self):
-        if self.constant == 0:
-            const = []
-        else:
-            const = [self.constant]
-        return ' + '.join(map(str, self.terms + const))
+        return ' + '.join(map(str, self.all_terms))
+    
+    @property
+    def all_terms(self):
+        for e in iter(self.terms):
+            yield e
+        if self.constant != 0: yield self.constant
     
     def isconstant(self):
         if self.terms: return False
@@ -269,7 +122,9 @@ class Term(Expression):
         self.const_coeff = constant
         self.params = list_of_params
         self.variable = variable
-        super(Term,self).__init__(0,[self])
+        bucket = TermBucket()
+        bucket[variable] = self
+        super(Term,self).__init__(0,bucket)
     
     def __str__(self):
         if self.const_coeff == 0: return '0'
@@ -277,67 +132,60 @@ class Term(Expression):
         if self.variable:
             elems.append(self.variable)
         return '*'.join(map(str,elems))
+    
+    @property
+    def data(self):
+        return self.const_coeff, self.params, self.variable
 
 class _Variable(Term):
     def __init__(self, name):
         self.name = name
         super(_Variable,self).__init__(1,[],self.name)
     
-    def __str__(self): return self.name
+    #def __str__(self): return self.name
 
 class _Param(Term):
     def __init__(self, name):
         self.name = name
         super(_Param,self).__init__(1,[self.name],'')
     
-    def __str__(self): return self.name
+    #def __str__(self): return self.name
 
 class _Constant(Expression):
     def __init__(self, value):
-        super(_Constant,self).__init__(value,[])
+        super(_Constant,self).__init__(value,TermBucket())
     
-    def __str__(self): return str(self.constant)
+    #def __str__(self): return str(self.constant)
 
 class _Add(Expression):
     def __init__(self, left, right):
-        # no simplification
-        # TODO: search through left.terms and right.terms via variable names
-        # terms ought to be a "dict" or somesuch...
-        #
-        # create our own "mapping"-like thing that allows list addition
-        #
-        # a+a+a -> 3*a
-        # a*c + a*c + a*c -> 3*a*c
-        # a*c + b*c -> a*c + b*c
-        #
-        # this needs to work for params too
         super(_Add, self).__init__(left.constant + right.constant, left.terms + right.terms)
 
 class _Mul(Expression):
     def __init__(self, left, right):
         # performs a distributed multiplication
-        newlist = []
+        newlist = TermBucket()
         
         # multiply out the constants
-        for l in left.terms:
-            coeff, params, variable = l.const_coeff, l.params, l.variable
-            newlist.append(Term(coeff*right.constant, params, variable))
-        for r in right.terms:
-            coeff, params, variable = r.const_coeff, r.params, r.variable
-            newlist.append(Term(coeff*left.constant, params, variable))
+        for l in iter(left.terms):
+            coeff, params, variable = l.data
+            newlist[variable] = Term(coeff*right.constant, params, variable)
+        for r in iter(right.terms):
+            coeff, params, variable = r.data
+            newlist[variable] = Term(coeff*left.constant, params, variable)
             
         # now multiply out the rest
-        for l in left.terms:
-            for r in right.terms:
-                lcoeff, lparams, lvariable = l.const_coeff, l.params, l.variable
-                rcoeff, rparams, rvariable = r.const_coeff, r.params, r.variable
+        for l in iter(left.terms):
+            for r in iter(right.terms):
+                lcoeff, lparams, lvariable = l.data
+                rcoeff, rparams, rvariable = r.data
                 if lvariable and rvariable:
                     raise ValueError("Cannot multiply two variable expressions")
                 if lvariable:
                     variable = lvariable
                 else:
                     variable = rvariable
-                newlist.append(Term(lcoeff*rcoeff, lparams + rparams, variable))
+                newlist[variable] = Term(lcoeff*rcoeff, lparams + rparams, variable)
 
         super(_Mul, self).__init__(left.constant * right.constant, newlist)
         
@@ -360,6 +208,8 @@ print y3*y2
 y4 = _Variable('x') + _Variable('x')
 print y4
 
+y5 = _Constant(6)*_Param('b') + _Constant(2.3)*_Variable('x') + _Param('b') + _Variable('x')
+print y5 + y4
     #__repr__ = __str__
 # x = Variable('x')
 # y = Number(1)
