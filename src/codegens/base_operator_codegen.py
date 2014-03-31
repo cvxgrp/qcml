6ab4da91bdfc5535e83cab92ebedae14436bed9d
Eric Chu <eric@qadium.com>
2014-03-31 12:11:23 -0700

    added actual operator code

---
+++ src/codegens/base_operator_codegen.py
@@ -0,0 +1,222 @@
+from base_codegen import Codegen
+from .. ast.constraints import  SOC, SOCProd, LinearConstraint
+from .. properties.shape import isscalar
+from .. properties.curvature import isconstant
+from .. codes import ConstantCoeff, ScalarParameterCoeff, ParameterCoeff, \
+    EyeCoeff, OnesCoeff
+
+from abc import ABCMeta, abstractmethod, abstractproperty
+import os
+
+def write_file(new_file, code):
+    with open(new_file, 'w') as output:
+        output.write(code)
+
+""" Operator codegen template.
+"""
+class OperatorCodegen(Codegen):
+    __metaclass__ = ABCMeta
+    """ Abstract class. Generates code for evaluating a linear operator and
+        its adjoint (transpose).
+
+        Since the cone program contains the matrix A for linear equality
+        constraints and G for conic inequality constraints, this codegen
+        will produce code for fA, fG, fAT, fGT, for evaluating the operator
+        A, G, A^T, and G^T, respectively.
+
+        Does not depend on any known data types or programming language.
+
+---
+        It generates code by calling the following sequence of functions:
+
+        function_prototype
+            creates the prototype for the function
+        function_preamble
+            any global setup, etc. code
+        function_datastructures
+            specifically, set up the data structures containing the problem data
+            c, G, h, A, b, and dims
+
+        function_solve
+            the specific call to the solver
+        function_recover
+            how to recover the solution from the solver
+
+        Before calling "function_solve", we make repeated calls to
+
+        function_stuff_c, function_stuff_h, function_stuff_b
+        function_stuff_G, function_stuff_A
+
+        These functions stuff the data into the datastructures set up in
+        function_datastructures.
+    """
+
+    def __init__(self):
+        """ Walks the tree and creates the data structures.
+            Creates the functions to stuff and un-stuff the matrices.
+        """
+        super(OperatorCodegen, self).__init__()
+
+    @abstractproperty
+    def fA(self):
+        """ Access the fA Function object
+        """
+        pass
+
+    @abstractproperty
+    def fAT(self):
+        """ Access the fAT Function object
+        """
+        pass
+
+    @abstractproperty
+    def fG(self):
+        """ Access the fG Function object
+        """
+        pass
+
+    @abstractproperty
+    def fGT(self):
+        """ Access the fGT Function object
+        """
+        pass
+
+    @abstractmethod
+    def stuff_G(self, row_start, row_end, col_start, col_end, expr, row_stride = None):
+        """ This code stuffs a *function*. Compare to base_codegen, where this code
+            is supposed to stuff a *matrix*.
+        """
+        yield ""
+
+    @abstractmethod
+    def stuff_A(self, row_start, row_end, col_start, col_end, expr, row_stride = None):
+        """ This code stuffs a *function*. Compare to base_codegen, where this code
+            is supposed to stuff a *matrix*.
+        """
+        yield ""
+
+    def codegen(self):
+        # create the source code
+        self.fA.create()
+        self.fAT.create()
+        self.fG.create()
+        self.fGT.create()
+        super(OperatorCodegen, self).codegen()
+    
+    """
+        # essentially the same as base_codegen except that i have to handle fA, fAT, etc.
+    """
+    def visit_LinearConstraint(self, node):
+        # in canonical form, all relop's are affine <= 0 or affine == 0
+        self.prob2socp.add_comment("for the constraint %s" % node)
+
+        if node.op == '==':
+            self.fA.add_comment("for the constraint %s" % node)
+            self.fAT.add_comment("for the constraint %s" % node)
+            start = self.num_lineqs
+            self.num_lineqs += node.shape.size(abstractdim_rewriter=self.abstractdim_rewriter)
+        else:
+            self.fG.add_comment("for the constraint %s" % node)
+            self.fGT.add_comment("for the constraint %s" % node)
+            start = self.num_lps
+            self.num_lps += node.shape.size(abstractdim_rewriter=self.abstractdim_rewriter)
+
+        self.generic_visit(node)
+
+        # nothing is on RHS
+        #right = self.expr_stack.pop()
+        left = self.expr_stack.pop()
+
+        # copy into the appropriate block
+        for k,v in left.iteritems():
+            if k == '1':
+                if node.op == '==':
+                    self.prob2socp.add_lines(self.stuff_b(start, self.num_lineqs, -v))
+                else:
+                    self.prob2socp.add_lines(self.stuff_h(start, self.num_lps, -v))
+            else:
+                xstart = self.varstart[k]
+                xend = xstart + self.varlength[k]
+                if node.op == '==':
+                    self.stuff_A(start, self.num_lineqs, xstart, xend, v)
+                else:
+                    self.stuff_G(start, self.num_lps, xstart, xend, v)
+
+        self.prob2socp.newline()
+        assert (not self.expr_stack), "Expected empty expression stack but still has %s left" % self.expr_stack
+
+    def visit_LinearEquality(self, node):
+        self.visit_LinearConstraint(node)
+
+    def visit_LinearInequality(self, node):
+        self.visit_LinearConstraint(node)
+
+    def visit_SOC(self, node):
+        self.prob2socp.add_comment("for the SOC constraint %s" % node)
+        self.fG.add_comment("for the SOC constraint %s" % node)
+        self.fGT.add_comment("for the SOC constraint %s" % node)
+
+        # we assume linear constraints have already been handled
+        start = [self.num_lps + self.num_conic]
+
+        start += [start[-1] + node.right.shape.size(abstractdim_rewriter=self.abstractdim_rewriter)]
+        cone_length = node.right.shape.size(abstractdim_rewriter=self.abstractdim_rewriter)
+        for e in node.left:
+            start += [start[-1] + e.shape.size(abstractdim_rewriter=self.abstractdim_rewriter)]
+            cone_length += e.shape.size(abstractdim_rewriter=self.abstractdim_rewriter)
+
+        self.num_conic += cone_length
+        self.cone_list.append( (1, str(cone_length)) )
+
+        self.generic_visit(node)
+
+        # copy into the appropriate block
+        while self.expr_stack:
+            e = self.expr_stack.pop()
+            coneend = start.pop()
+            conestart = start[-1]   # peek at top
+
+            for k,v in e.iteritems():
+                if k == '1':
+                    self.prob2socp.add_lines(self.stuff_h(conestart, coneend, v))
+                else:
+                    xstart = self.varstart[k]
+                    xend = xstart + self.varlength[k]
+                    self.stuff_G(conestart, coneend, xstart, xend, -v)
+
+        self.prob2socp.newline()
+        assert (not self.expr_stack), "Expected empty expression stack but still has %s left" % self.expr_stack
+
+    def visit_SOCProd(self, node):
+        self.prob2socp.add_comment("for the SOC product constraint %s" % node)
+        self.fG.add_comment("for the SOC product constraint %s" % node)
+        self.fGT.add_comment("for the SOC product constraint %s" % node)
+
+        # we assume linear constraints have already been handled
+        start = self.num_lps + self.num_conic
+        stride = node.nargs + 1
+        self.num_conic += stride * node.shape.size(abstractdim_rewriter=self.abstractdim_rewriter)
+
+        self.cone_list.append( (node.shape.size(abstractdim_rewriter=self.abstractdim_rewriter), str(node.nargs + 1)) )
+
+        self.generic_visit(node)
+
+        # copy into the appropriate block
+        count = stride - 1
+        coneend = self.num_conic + self.num_lps
+
+        while self.expr_stack:
+            e = self.expr_stack.pop()
+            conestart = start + count
+            count -= 1
+            for k,v in e.iteritems():
+                if k == '1':
+                    self.prob2socp.add_lines(self.stuff_h(conestart, coneend, v, stride))
+                else:
+                    xstart = self.varstart[k]
+                    xend = xstart + self.varlength[k]
+                    self.stuff_G(conestart, coneend, xstart, xend, -v, stride)
+
+        self.prob2socp.newline()
+        assert (not self.expr_stack), "Expected empty expression stack but still has %s left" % self.expr_stack
+
---
+++ src/codegens/python/operator_codegen.py
@@ -0,0 +1,172 @@
+from .. base_operator_codegen import OperatorCodegen
+from ... codes import OnesCoeff, ConstantCoeff
+from ... codes.function import PythonFunction
+from ... codes.encoders import toPython
+from ... properties.abstract_dim import AbstractDim
+
+def wrap_self(f):
+    def wrapped_code(self, *args, **kwargs):
+        return f(*args, **kwargs)
+    return wrapped_code
+
+class PythonOperatorCodegen(OperatorCodegen):
+    def __init__(self):
+        super(PythonOperatorCodegen, self).__init__()
+        self._code = {
+            'fA': PythonFunction('fA', ['x']),
+            'fG': PythonFunction('fG', ['x']),
+            'fAT': PythonFunction('fAT', ['y']),
+            'fGT': PythonFunction('fGT', ['y']),
+            'prob2socp': PythonFunction('prob_to_socp', ['params', 'dims={}']),
+            'socp2prob': PythonFunction('socp_to_prob', ['x', 'dims={}']),
+        }
+        self._codekeyorder = ['fA', 'fG', 'fAT', 'fGT', 'prob2socp', 'socp2prob']
+
+    @property
+    def prob2socp(self):
+        return self.code['prob2socp']
+
+    @property
+    def socp2prob(self):
+        return self.code['socp2prob']
+
+    @property
+    def fA(self):
+        return self.code['fA']
+
+    @property
+    def fAT(self):
+        return self.code['fAT']
+
+    @property
+    def fG(self):
+        return self.code['fG']
+
+    @property
+    def fGT(self):
+        return self.code['fGT']
+        
+    @property
+    def extension(self):
+        return ".py"
+
+    # function to get problem dimensions
+    def python_dimensions(self):
+        yield "p, m, n = %s, %s, %s" % (self.num_lineqs, self.num_conic + self.num_lps, self.num_vars)
+
+    # function to get cone dimensions
+    def python_cone_sizes(self):
+        def cone_tuple_to_str(x):
+            num, sz = x
+            if num == 1: return "[%s]" % sz
+            else: return "%s*[%s]" % (num, sz)
+        cone_list_str = '[]'
+        if self.cone_list:
+            cone_list_str = map(cone_tuple_to_str, self.cone_list)
+            cone_list_str = '+'.join(cone_list_str)
+
+        yield "cones = {'l': %s, 'q': %s, 's': []}" % (self.num_lps, cone_list_str)
+
+    def functions_setup(self):
+        # add some documentation
+        self.prob2socp.document("maps 'params' into a dictionary of SOCP data")
+        self.prob2socp.document("'params' ought to contain:")
+        self.prob2socp.document(self.printshapes(self.program))
+
+        # now import cvxopt and itertools
+        self.prob2socp.add_lines("import numpy as np")
+        self.prob2socp.add_lines("import scipy.sparse as sp")
+        self.prob2socp.add_lines("import itertools")
+        self.prob2socp.newline()
+        # self.prob2socp.add_comment("convert possible numpy parameters to cvxopt matrices")
+        # self.prob2socp.add_lines("from qcml.helpers import convert_to_cvxopt")
+        # self.prob2socp.add_lines("params = convert_to_cvxopt(params)")
+        # self.prob2socp.newline()
+
+        # set up the data structures
+        self.prob2socp.add_lines(self.python_dimensions())
+        self.prob2socp.add_lines("c = np.zeros((n,))")
+        self.prob2socp.add_lines("h = np.zeros((m,))")
+        self.prob2socp.add_lines("b = np.zeros((p,))")
+        # self.prob2socp.add_lines("Gi, Gj, Gv = [], [], []")
+        # self.prob2socp.add_lines("Ai, Aj, Av = [], [], []")
+        self.prob2socp.add_lines(self.python_cone_sizes())
+
+        self.fA.add_lines("y = np.zeros((p,))")
+        self.fAT.add_lines("x = np.zeros((n,))")
+        self.fG.add_lines("y = np.zeros((m,))")
+        self.fGT.add_lines("x = np.zeros((n,))")
+
+    def functions_return(self):
+        # TODO: what to do when m, n, or p is 0?
+        # it "just worked" with CVXOPT, but not with scipy/numpy anymore...
+        # self.prob2socp.add_comment("construct index and value lists for G and A")
+        # self.prob2socp.add_lines("Gi = np.fromiter(itertools.chain.from_iterable(Gi), dtype=np.int)")
+        # self.prob2socp.add_lines("Gj = np.fromiter(itertools.chain.from_iterable(Gj), dtype=np.int)")
+        # self.prob2socp.add_lines("Gv = np.fromiter(itertools.chain.from_iterable(Gv), dtype=np.double)")
+        # self.prob2socp.add_lines("Ai = np.fromiter(itertools.chain.from_iterable(Ai), dtype=np.int)")
+        # self.prob2socp.add_lines("Aj = np.fromiter(itertools.chain.from_iterable(Aj), dtype=np.int)")
+        # self.prob2socp.add_lines("Av = np.fromiter(itertools.chain.from_iterable(Av), dtype=np.double)")
+        # self.prob2socp.add_lines("if m > 0: G = sp.csc_matrix((Gv, np.vstack((Gi, Gj))), (m,n))")
+        # self.prob2socp.add_lines("else: G, h = None, None")
+        # self.prob2socp.add_lines("if p > 0: A = sp.csc_matrix((Av, np.vstack((Ai, Aj))), (p,n))")
+        # self.prob2socp.add_lines("else: A, b = None, None")
+
+        self.fA.add_lines("return y")
+        self.fAT.add_lines("return x")
+        self.fG.add_lines("return y")
+        self.fGT.add_lines("return x")
+
+        self.prob2socp.add_lines(self.fA.code)
+        self.prob2socp.newline()
+        self.prob2socp.add_lines(self.fAT.code)
+        self.prob2socp.newline()
+        self.prob2socp.add_lines(self.fG.code)
+        self.prob2socp.newline()
+        self.prob2socp.add_lines(self.fGT.code)
+        self.prob2socp.newline()
+
+        self.prob2socp.add_lines("return {'c': c, 'G': fG, 'GT': fGT, 'h': h, 'A': fA, 'AT': fAT, 'b': b, 'dims': cones}")
+
+        self.socp2prob.document("recovers the problem variables from the solver variable 'x'")
+        # recover the old variables
+        recover = (
+            "'%s' : x[%s:%s]" % (k, self.varstart[k], self.varstart[k]+self.varlength[k])
+                for k in self.program.variables.keys()
+        )
+        self.socp2prob.add_lines("return {%s}" % ', '.join(recover))
+
+    def stuff_c(self, start, end, expr):
+        yield "c[%s:%s] = %s" % (start, end, toPython(expr))
+
+    def stuff_b(self, start, end, expr):
+        yield "b[%s:%s] = %s" % (start, end, toPython(expr))
+
+    def stuff_h(self, start, end, expr, stride = None):
+        if stride is not None:
+            yield "h[%s:%s:%s] = %s" % (start, end, stride, toPython(expr))
+        else:
+            yield "h[%s:%s] = %s" % (start, end, toPython(expr))
+
+    def stuff_function(self, func, rstart, rend, cstart, cend, expr, rstride):
+        n = (rend - rstart) / rstride
+        if (isinstance(n, AbstractDim) or n > 1) and expr.isscalar:
+            expr = OnesCoeff(n,ConstantCoeff(1))*expr
+        self.code[func].add_lines("y[%s:%s:%s] += %s * x[%s:%s]" % (rstart, rstride, rend, toPython(expr), cstart, cend))
+        self.code[func + 'T'].add_lines("x[%s:%s] += %s * y[%s:%s:%s]" % (cstart, cend, toPython(expr), rstart, rstride, rend))
+        #yield expr
+
+        # to_sparse = expr.to_sparse()
+        # if to_sparse: yield toPython(to_sparse)
+        # yield "%si.append(%s)" % (mat, toPython(expr.I(rstart, rstride)))
+        # yield "%sj.append(%s)" % (mat, toPython(expr.J(cstart)))
+        # yield "%sv.append(%s)" % (mat, toPython(expr.V()))
+
+    def stuff_G(self, rstart, rend, cstart, cend, expr, rstride = 1):
+        self.stuff_function("fG", rstart, rend, cstart, cend, expr, rstride)
+
+    def stuff_A(self, rstart, rend, cstart, cend, expr, rstride = 1):
+        self.stuff_function("fA", rstart, rend, cstart, cend, expr, rstride)
+
+    def abstractdim_rewriter(self, ad):
+        return "dims['%s']" % ad
